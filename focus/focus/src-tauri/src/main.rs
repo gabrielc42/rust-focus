de21a9a38171a137cda4ef8e3ead09b2f2307592
gabrielc42 <clark.gabe97@gmail.com>
2022-11-07 16:21:52 -0600

    file save and updates with enter key, greet button not working, unsure how

--- focus/focus/src-tauri/src/main.rs
+++ focus/focus/src-tauri/src/main.rs
@@ -1,18 +1,32 @@
 #![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
-  )]use std::fs::OpenOptions;
-  use std::io::prelude::*;#[tauri::command]
-  fn add_task(content: String) {
+)]
+
+use std::env;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn add_task(content: String) {
     let mut file = OpenOptions::new()
-      .create(true)
-      .append(true)
-      .open("../tasks.txt")
-      .expect("Error while opening the tasks file");
+        .create(true)
+        .append(true)
+        .open("../tasks.txt")
+        .expect("Error while opening the tasks file");
     writeln!(file, "{}", content).expect("Error while writing in the tasks file");
-  }fn main() {
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
     tauri::Builder::default()
-      .invoke_handler(tauri::generate_handler![add_task])
-      .run(tauri::generate_context!())
-      .expect("error while running tauri application");
-  }
+        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![add_task])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
